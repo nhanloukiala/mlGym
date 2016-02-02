7dd2ca605c53308db7eee7a174e8c4a03d76ba5a
Nejn <tantrokiniem@gmail.com>
2016-02-02 21:05:22 +0200

    adding different N for leave n out

--- metal_ion_prediction.py
+++ metal_ion_prediction.py
@@ -16,20 +16,25 @@ from sklearn.svm import SVR
 import pdb
 
 data = pd.read_csv('Water_data.csv')
+# Split data into labels and features
 train_labels, train_data = np.hsplit(data, [3])
+
+# Normalization
 train_data = standardize_dataset(train_data)
 train_labels = train_labels.as_matrix()
 
-plot_data = []
+#Try with different neighbors and different leave N out cross validation:
+for n in [1, 3]:
+    plot_data = []
 
-for i in range(1, 30, 1):
-    model = NearestNeighborsRegressor(n_neighbors = i)
-    runner = LeaveNOut()
-    predict, true = runner.run(data=train_data, model=model, labels=train_labels, n_out=3)
-    score = Score()
+    for i in range(1, 30, 1):
+        model = NearestNeighborsRegressor(n_neighbors=i)
+        runner = LeaveNOut()
+        predict, true = runner.run(data=train_data, model=model, labels=train_labels, n_out=n)
+        score = Score()
+        plot_data.append([score.c_score(np.array(predict)[:, 0], np.array(true)[:, 0]), i, 'c_total'])
+        plot_data.append([score.c_score(np.array(predict)[:, 1], np.array(true)[:, 1]), i, 'Cd'])
+        plot_data.append([score.c_score(np.array(predict)[:, 2], np.array(true)[:, 2]), i, 'Pb'])
 
-    plot_data.append([score.c_score(np.array(predict)[:,0], np.array(true)[:,0]), i, 'c_total'])
-    plot_data.append([score.c_score(np.array(predict)[:,1], np.array(true)[:,1]), i, 'Cd'])
-    plot_data.append([score.c_score(np.array(predict)[:,2], np.array(true)[:,2]), i, 'Pb'])
-
-line_plot(np.array(plot_data))
+    line_plot(np.array(plot_data), title="C_index by different K Neighbors - Leave %s out CV" % n,
+              x_title="K Neighbors", y_title="C-Index")
